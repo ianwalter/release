52d010420c6099f424cb8a754a00a714619ab206
Ian Walter <public@iankwalter.com>
2019-07-12 22:15:42 -0400

    Adding procedure

--- cli.js
+++ cli.js
@@ -3,33 +3,42 @@
 const cli = require('@ianwalter/cli')
 const { print } = require('@ianwalter/print')
 const prompts = require('prompts')
+const semver = require('semver')
 const release = require('.')
 
 async function run () {
-  const config = cli({ name: 'release' })
+  const { $package, ...config } = cli({ name: 'release' })
 
   if (config._.length) {
     config.version = config.length[1]
     delete config._
   }
 
-  if (!config.version) {
+  if (config.version) {
+    // TODO: check if it's higher than current version
+  } else {
+    const patch = semver.inc($package.version, 'patch')
+    const minor = semver.inc($package.version, 'minor')
+    const major = semver.inc($package.version, 'major')
+    const prePatch = semver.inc($package.version, 'prepatch')
+    const preMinor = semver.inc($package.version, 'preminor')
+    const preMajor = semver.inc($package.version, 'premajor')
     Object.assign(config, await prompts({
       type: 'select',
       name: 'version',
       message: 'Select the semantic version to publish',
       choices: [
-        { title: 'Patch', value: '#ff0000' },
-        { title: 'Minor', value: '#00ff00' },
-        { title: 'Major', value: '#0000ff' },
-        { title: 'Pre-patch', value: '#0000ff' },
-        { title: 'Pre-minor', value: '#0000ff' },
-        { title: 'Pre-major', value: '#0000ff' }
+        { title: `Patch\t${patch}`, value: patch },
+        { title: `Minor\t${minor}`, value: minor },
+        { title: `Major\t${major}`, value: major },
+        { title: `Pre-patch\t${prePatch}`, value: prePatch },
+        { title: `Pre-minor\t${preMinor}`, value: preMinor },
+        { title: `Pre-major\t${preMajor}`, value: preMajor }
       ]
     }))
   }
 
-  await release(config)
+  await release({ $package, ...config })
 
   print.log(config)
 }
--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+const { promises: fs } = require('fs')
 const { print } = require('@ianwalter/print')
 const execa = require('execa')
+const newGithubReleaseUrl = require('new-github-release-url')
 
 module.exports = async ({ $package, ...config }) => {
   // Run the test script if it is defined in the project's package.json.
   if ($package.scripts.test) {
     await execa('yarn', ['test'])
   }
+
+  // Checkout master.
+  await execa('git', ['checkout', 'master'])
+
+  // Pull the latest from upstream.
+  await execa('git', ['pull'])
+
+  // Checkout a release branch.
+  const tag = `v${config.version}`
+  await execa('git', ['checkout', '-b', `release-${tag}`])
+
+  // Update the package.json version.
+  $package.version = config.version
+  await fs.writeFile(path.resolve('package.json', ))
+
+  // Commit the version update.
+  await execa('git', ['commit', '-m', tag])
+
+  // Push the release branch upstream.
+  await execa('git', ['push', '-u'])
+
+  // Publish the package.
+  await execa('yarn', 'publish')
+
+  // Create the release on GitHub.
+  const githubUrl = newGithubReleaseUrl({
+    tag,
+    isPrerelease: config.isPrerelease
+  })
+
+  print.success('', githubUrl)
 }
