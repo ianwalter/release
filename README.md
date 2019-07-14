4d574af52ed79b849a5f316c2052d04a1e2a5bd6
Ian Walter <public@iankwalter.com>
2019-07-14 16:58:44 -0400

    v0.0.0

--- README.md
+++ README.md
@@ -3,5 +3,14 @@
 
 ## About
 
-Heavily inspired by [np][npUrl] but with a focus on `yarn` and excellent support
-for private registries.
+HEAVILY inspired by [np][npUrl] but with a focus on `yarn` and better support
+for branch protections and private registries.
+
+1. Display commits and prompt for semver.
+2. Check and fail if upstream has commits.
+3. Run tests.
+4. If branch is specified, create a release branch, push it, and prompt for
+   publish confirmation.
+5. Publish package and display GitHub release URL.
+
+If --yolo is specified, just publish the package and display GitHub release URL.
--- cli.js
+++ cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const cli = require('@ianwalter/cli')
+const execa = require('execa')
 const { print } = require('@ianwalter/print')
 const prompts = require('prompts')
 const semver = require('semver')
@@ -14,6 +15,8 @@ async function run () {
     delete config._
   }
 
+  await execa('commits', [`v${$package.version}`], { stdio: 'inherit' })
+
   if (config.version) {
     // TODO: check if it's higher than current version
   } else {
@@ -23,19 +26,40 @@ async function run () {
     const prePatch = semver.inc($package.version, 'prepatch')
     const preMinor = semver.inc($package.version, 'preminor')
     const preMajor = semver.inc($package.version, 'premajor')
-    Object.assign(config, await prompts({
+
+    const { version } = await prompts({
       type: 'select',
       name: 'version',
       message: 'Select the semantic version to publish',
       choices: [
-        { title: `Patch\t${patch}`, value: patch },
-        { title: `Minor\t${minor}`, value: minor },
-        { title: `Major\t${major}`, value: major },
-        { title: `Pre-patch\t${prePatch}`, value: prePatch },
-        { title: `Pre-minor\t${preMinor}`, value: preMinor },
-        { title: `Pre-major\t${preMajor}`, value: preMajor }
+        {
+          title: `Patch\t${patch}`,
+          value: { version: patch }
+        },
+        {
+          title: `Minor\t${minor}`,
+          value: { version: minor }
+        },
+        {
+          title: `Major\t${major}`,
+          value: { version: major }
+        },
+        {
+          title: `Pre-patch\t${prePatch}`,
+          value: { version: prePatch, isPrerelease: true }
+        },
+        {
+          title: `Pre-minor\t${preMinor}`,
+          value: { version: preMinor, isPrerelease: true }
+        },
+        {
+          title: `Pre-major\t${preMajor}`,
+          value: { version: preMajor, isPrerelease: true }
+        }
       ]
-    }))
+    })
+
+    Object.assign(config, version)
   }
 
   await release({ $package, ...config })
